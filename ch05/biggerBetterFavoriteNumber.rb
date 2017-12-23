2b5bd330e7e7cd8b8e4eced5a7dfdfced49e2a32
Sophia Choi <s.choi2464@gmail.com>
2017-12-22 21:09:37 -0800

    Add ch05

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,6 @@
+puts 'Hi there! ' + 'We\'re going to play a game.' + ' What\'s your favorite number?'
+favorite = gets.to_f
+puts 'Mmm I didn\'t quite get that. Could you enter your number again?'
+puts 1 + gets.to_f
+puts 'I think this number is better. It is scientifically proven that bigger numbers are always better.' 
+puts 'Anyway, thanks for playing! :)'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,8 @@
+puts 'Hi there! ' + 'What is your first name?'
+first = gets.chomp
+puts 'What is your middle name?'
+middle = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+puts 'Your name is ' + first + ' ' + middle + ' ' + last + '? That\'s cool!' 
+puts 'Nice to meet you, ' + first + ' ' + middle + ' ' + last + '. :)'
