c3463a131dcc567435e2d94d495ca86465950554
Sophia Choi <s.choi2464@gmail.com>
2018-01-02 20:16:53 -0800

    Add ch05

--- ch05/biggerBetterFavoriteNumber.rb
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -1,6 +1,5 @@
 puts 'Hi there! ' + 'We\'re going to play a game.' + ' What\'s your favorite number?'
 favorite = gets.to_f
-puts 'Mmm I didn\'t quite get that. Could you enter your number again?'
-puts 1 + gets.to_f
+puts 1 + favorite 
 puts 'I think this number is better. It is scientifically proven that bigger numbers are always better.' 
 puts 'Anyway, thanks for playing! :)'
